f055480ab9d2060716d61e04f0e0a65bf66e8d2f
Adrian Almeida <adrianalmeida417@gmail.com>
2026-09-05 15:10:38 -0300

    chore: <nvim> Adding shuck LSP config

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,10 +3,3 @@ require("config.keymaps")
 require("config.settings")
 
 require("core.lazy")
-
-	-- Configurações Pluigns
---require("mason-config")
---require("telescope-config")
---require("vgit-config")
---require("coc-config")
---require("dap-config");
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -3,39 +3,45 @@ local language_servers = {
   "clangd",
   "docker_language_server",
   "jdtls",
-  "ast_grep",
+  "shuck",
 }
+
+local configure_lsp = function()
+  local lspconfig = vim.lsp.config
+  local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+  for _, language in pairs(language_servers) do
+    lspconfig(language, { capabilities = capabilities })
+  end
+
+  lspconfig("dartls", {
+    dartls = {
+      cmd = { "dart", "language-server", "--protocol=lsp" },
+    },
+    capabilities = capabilities,
+  })
+
+  lspconfig("shuck", {
+    cmd = { "shuck", "server" },
+    filetypes = { "sh", "bash", "zsh", "ksh" },
+    root_markers = { ".shuck.toml", "shuck.toml", ".git" },
+    capabilities = capabilities,
+  })
+end
 
 return {
   {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = language_servers,
       })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = vim.lsp.config
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      for _, language in pairs(language_servers) do
-        lspconfig(language, { capabilities = capabilities })
-      end
-
-      lspconfig("dartls", {
-        dartls = {
-          cmd = { "dart", "language-server", "--protocol=lsp" },
-        }
-      })
+      configure_lsp()
 
       local opts = { noremap = true, silent = true, nowait = true }
       local keymap = vim.keymap.set
@@ -49,5 +55,5 @@ return {
 
       keymap("i", "<A-CR>", vim.lsp.buf.code_action, opts)
     end,
-  }
+  },
 }
